03a66a1cea08945f8b19cf2895b05b2a74f5d9ab
Angelinagustin <angelinagustin_2226240035@mhs.mdp.ac.id>
2024-11-20 18:26:33 +0700

    ADD: Perbaikan
    .

--- belajar-Angular/src/app/housing-location.ts
+++ belajar-Angular/src/app/housing-location.ts
@@ -1,11 +1,10 @@
 export interface HousingLocation {
-    id: number,
-    nama: string,
-    city: string,
-    state : string,
-    photo : string,
-    availabelUnits : number,
-    wifi : boolean,
-    laundry : boolean
-};
-// merupakan models dari monggo db
+  id: number,
+  name: string,
+  city: string,
+  state: string,
+  photo: string,
+  availableUnits: number,
+  wifi: boolean,
+  laundry: boolean
+}
